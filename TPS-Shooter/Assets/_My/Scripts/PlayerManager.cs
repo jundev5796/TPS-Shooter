7f011f6f5e124a18f038f218ceb9735b07dc9305
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-26 15:12:34 +0900

    Configure target aim ui

--- TPS-Shooter/Assets/_My/Scripts/PlayerManager.cs
+++ TPS-Shooter/Assets/_My/Scripts/PlayerManager.cs
@@ -9,6 +9,9 @@ public class PlayerManager : MonoBehaviour
     [Header("Aim")]
     [SerializeField] private CinemachineVirtualCamera aimCam;
     [SerializeField] private GameObject aimImage;
+    [SerializeField] private GameObject aimObj;
+    [SerializeField] private float aimObjDis = 10f;
+    [SerializeField] private LayerMask targetLayer;
 
     void Start()
     {
@@ -26,6 +29,28 @@ public class PlayerManager : MonoBehaviour
         {
             aimCam.gameObject.SetActive(true);
             aimImage.SetActive(true);
+
+            Vector3 targetPosition = Vector3.zero;
+            Transform camTransform = Camera.main.transform;
+            RaycastHit hit;
+
+            if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, Mathf.Infinity, targetLayer))
+            {
+                //Debug.Log("Name: " + hit.transform.gameObject.name);
+                targetPosition = hit.point;
+                aimObj.transform.position = hit.point;
+            }
+            else
+            {
+                targetPosition = camTransform.position + camTransform.forward * aimObjDis;
+                aimObj.transform.position = camTransform.position + camTransform.forward * aimObjDis;
+            }
+
+            Vector3 targetAim = targetPosition;
+            targetAim.y = transform.position.y;
+            Vector3 aimDir = (targetAim - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDir, Time.deltaTime * 50f);
         }
         else
         {
